c75d45c4ab1a822ebc731e0c7b60e7f769a0e32d
DmitriyR90 <ryabokon.dn@gmail.com>
2022-10-23 22:02:26 +0300

    part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { About } from '../pages/About';
-import { Home } from '../pages/Home';
-import { ProductDetails } from '../pages/ProductDetails';
-import { Products } from '../pages/Products';
-import { NotFound } from '../pages/NotFound';
-
-import { Mission } from './Mission';
-import { Team } from './Team';
-import { Reviews } from './Reviews';
 import { SharedLayout } from './SharedLayout';
+
+const About = lazy(() => import('../pages/About'));
+const Home = lazy(() => import('../pages/Home'));
+const ProductDetails = lazy(() => import('../pages/ProductDetails'));
+const Products = lazy(() => import('../pages/Products'));
+const Mission = lazy(() => import('../components/Mission'));
+const Team = lazy(() => import('../components/Team'));
+const Reviews = lazy(() => import('../components/Reviews'));
 
 export const App = () => {
   return (
@@ -22,8 +22,36 @@ export const App = () => {
         </Route>
         <Route path="products" element={<Products />} />
         <Route path="products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />;
       </Route>
     </Routes>
   );
 };
+/*
+
+const createAsyncComponent = path => lazy(() => import(path));
+
+const About = createAsyncComponent('../pages/About');
+const Home = createAsyncComponent('../pages/Home');
+const ProductDetails = createAsyncComponent('../pages/ProductDetails');
+const Products = createAsyncComponent('../pages/Products');
+const Mission = createAsyncComponent('./Mission');
+const Team = createAsyncComponent('./Team');
+const Reviews = createAsyncComponent('./Reviews');
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
+  );
+};
+*/
